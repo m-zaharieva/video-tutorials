78fb8fa393dd0b28c5f649dc68bc5162756eef88
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-28 17:11:50 +0300

    create basic user model

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    password: {
+    hashedPassword: {
         type: String,
         required: true,
     }
