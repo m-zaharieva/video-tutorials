5f0ef984b45d1f15d7e5fd48a45587ec9daf1622
Мирослава Захариева <miroslava.zaharieva@gmail.com>
2021-10-28 17:34:40 +0300

    create Course model

---
+++ src/models/Course.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    isPublic: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+    },
+    users: [
+        {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+        }
+    ]
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+module.exports = Course;
